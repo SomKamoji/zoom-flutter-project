36d3c619074dc5650dbfc04d7d3af44ac7d0a7c5
Som Kamoji <somkamoji01@gmail.com>
2022-06-12 00:48:47 +0530

    project for profile

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:zoom_clone_tutorial/resources/auth_methods.dart';
+import 'package:zoom_clone_tutorial/screens/history_meeting_screen.dart';
+import 'package:zoom_clone_tutorial/screens/meeting_screen.dart';
+import 'package:zoom_clone_tutorial/utils/colors.dart';
+import 'package:zoom_clone_tutorial/widgets/custom_button.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int  _page = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+  
+  // List<Widget> pages = [
+  //   MeetingScreen(),
+  //   const HistoryMeetingScreen(),
+  //   const Text('contacts'),
+  //   CustomButton(text: 'Log Out', onPressed: () =>  AuthMethods().signOut(context); 
+
+  //   ),
+  // ];
+
+
+  @override 
+  Widget build(BuildContext context) {
+    List<Widget> pages = [
+    MeetingScreen(),
+    const HistoryMeetingScreen(),
+    const Text('contacts'),
+    CustomButton(text: 'Log Out', onPressed: () =>  AuthMethods().signOut(context) 
+
+    ),
+  ];
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+      ),
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 14,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.comment_bank,
+              ),
+               label: 'Meet & Chat',
+               ),
+
+               BottomNavigationBarItem(                 
+                icon: Icon(
+                  Icons.lock_clock,
+                  ),
+                  label: 'Meetings',
+               ),
+
+               BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outline,
+                  ),
+                  label: 'Contacts',
+               ),
+
+               BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.settings_outlined,
+                  ),
+                  label: 'Settings',
+               ),                              
+           ], 
+      ),
+    );
+  }
+}
